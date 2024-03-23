054f44663c7adc00b01d554f02e30640aedddc9b
DarshanDixit05 <darshandixit0@gmail.com>
2024-03-23 22:55:44 +0530

    turbo repo, tailwind, prisma init

---
+++ apps/user-side/app/api/user/route.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@next-wallet/db/client';
+import { NextResponse } from 'next/server';
+
+const client = new PrismaClient();
+
+export const GET = async () => {
+    await client.user.create({
+        data: {
+            email: "asd",
+            name: "adsads"
+        }
+    })
+    return NextResponse.json({
+        message: "hi there"
+    })
+}
