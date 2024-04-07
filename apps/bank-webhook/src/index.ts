30b1365ed6c1259ebfd6841fbc24ca16169e4f48
DarshanDixit05 <darshandixit0@gmail.com>
2024-04-07 15:55:31 +0530

    added on ramp route

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,7 +1,44 @@
 import express from "express"
+import prisma from "@next-wallet/db/client"
 
 const app = express()
 
 app.post('/hdfcWebhook', async(req:any, res:any)=>{
-    
+    const paymentData = {
+        amount : req.body.amount,
+        userId : req.body.userId,
+        token : req.body.token
+    }
+
+    try {
+        await prisma.$transaction([
+            prisma.balance.update({
+                where:{
+                    userId:Number(paymentData.userId)
+                },
+                data:{
+                    amount : {
+                        increment : Number(paymentData.amount)
+                    }
+                }
+            }),
+            prisma.onRampTransaction.update({
+                where:{
+                    token:paymentData.token
+                },
+                data:{
+                    status:"Success"
+                }
+            })
+        ])
+
+        return res.status(200).json({
+            message:"Completed"
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
+    }
 })
